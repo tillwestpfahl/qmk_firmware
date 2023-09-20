ab497477705447a581340d91675782590039329f
Till Westpfahl <t.westpfahl@mittwald.de>
2023-09-20 09:35:09 +0200

    feat: first draft

---
+++ keyboards/splitkb/kyria/keymaps/tillwestpfahl/rules.mk
@@ -0,0 +1,12 @@
+# Bootloader selection
+BOOTLOADER = rp2040
+
+RP2040_BOOTLOADER_DOUBLE_TAP_RESET = yes
+
+# Convert from Elite-C to Liatris
+CONVERT_TO=liatris
+
+OLED_ENABLE = yes
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
