d2c3d47351a53f0e20099208eadcf4b98e7d5686
Till Westpfahl <t.westpfahl@mittwald.de>
2023-09-20 20:29:16 +0200

    feat: disable oled and rgb

--- keyboards/splitkb/kyria/keymaps/tillwestpfahl/rules.mk
+++ keyboards/splitkb/kyria/keymaps/tillwestpfahl/rules.mk
@@ -6,7 +6,7 @@ RP2040_BOOTLOADER_DOUBLE_TAP_RESET = yes
 # Convert from Elite-C to Liatris
 CONVERT_TO=liatris
 
-OLED_ENABLE = yes
+OLED_ENABLE = no
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
